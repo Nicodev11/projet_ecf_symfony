bcd2e4e5f9553aae6fb8b6e86efecabf64e9e8d5
Nicolas Gardien <Nicolasgardien.developpeur@gmail.com>
2023-03-01 02:11:50 +0100

    Add auth and register user in database + forms

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -4,10 +4,13 @@ namespace App\Entity;
 
 use App\Repository\UsersRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UsersRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class Users implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -38,6 +41,10 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column]
     private ?\DateTimeImmutable $created_at = null;
+
+    public function __construct() {
+        $this->created_at = new \DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
