73b15b63c952098b9b92a9f56491ba9d22f8e9d6
Nicolas Gardien <Nicolasgardien.developpeur@gmail.com>
2023-03-03 17:50:09 +0100

    Update last commit

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+import "../styles/app.scss";
+
+// start the Stimulus application
+import "../bootstrap";
+
+const btn = document.querySelector('.btn')
+
+console.log(btn)
--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,1 @@
 import '../styles/home.scss'
-
-const btn = document.querySelector('.btn')
-
-btn.addEventListener('click', (e) => {
-  console.log(e)
-})
